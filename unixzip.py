8492f53d7a7668b13002b1f7117ec05d6dd1b850
Unixtz <97262447+Iamunix@users.noreply.github.com>
2023-03-28 02:25:36 +0300

    Update unixzip.py

--- unixzip.py
+++ unixzip.py
@@ -10,28 +10,25 @@ res= pyg.figlet_format("Unix ZIP", font = "slant")
 print(res)
 print("#"*5,"created by unixcoder","#"*5)
 
+import zipfile
 
-# the password list path you want to use
-wordlist = sys.argv[2]
-# the zip file you want to crack its password
-zip_file = sys.argv[1]
-# initialize the Zip File object
-zip_file = zipfile.ZipFile(zip_file)
-# count the number of words in this wordlist
-n_words = len(list(open(wordlist, "rb")))
-# print the total number of passwords
-print("Total passwords to test:", n_words)
-with open(wordlist, "rb") as wordlist:
-    for word in tqdm(wordlist, total=n_words, unit="word"):
-        try:
-            zip_file.extractall(pwd=word.strip())
-        except:
-            continue
-        else:
-            print("[+] Password found:", word.decode().strip())
-            exit(0)
-print("[!] Password not found, try other wordlist.")
-print("")
+zip_file = input("Enter the name of the zip file: ")
+dictionary_file = input("Enter the name of the dictionary file: ")
+
+with open(dictionary_file, 'r') as f:
+    passwords = f.readlines()
+
+for password in passwords:
+    password = password.strip('\n')
+    try:
+        with zipfile.ZipFile(zip_file) as zf:
+            zf.extractall(pwd=password.encode())
+            print(f"Password found: {password}")
+            break
+    except:
+        pass
+
+
 print("Thanks for Hacking 😋")
 
 ############################################
