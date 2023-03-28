ea2b5f29dea69514e484691847eca3b1404ab1ee
Unixtz <97262447+Iamunix@users.noreply.github.com>
2023-03-29 00:14:45 +0300

    Add files via upload

---
+++ unixzip.py
@@ -0,0 +1,46 @@
+#!/use/bin/evn python3
+# https://github.com/Iamunix
+# Author - iamunix
+
+import sys
+
+
+# pretty banner to unixZIP Cracker
+
+print("""\033[1;96m
+ _   _       _        ______         _
+| | | |_ __ (_)_  __ |__  (_)_ __   | |
+| | | | '_ \| \ \/ /   / /| | '_ \  | |
+| |_| | | | | |>  <   / /_| | |_) | |_|
+ \___/|_| |_|_/_/\_\ /____|_| .__/  (_)
+                            |_| v1.0
+------------created by unixcoder------------
+ \033[0;0m""")
+
+zip_file = input("Enter the name of the zip file: ")
+dictionary_file = input("Enter the worldlist file: ")
+
+with open(dictionary_file, 'r') as f:
+    passwords = f.readlines()
+
+for password in passwords:
+    password = password.strip('\n')
+    try:
+        with zipfile.ZipFile(zip_file) as zf:
+            zf.extractall(pwd=password.encode())
+            print(f"Password found: {password}")
+            break
+    except:
+        pass
+
+
+print("Thanks for Hacking 😋")
+
+############################################
+############################################
+############################################
+################created by coder############
+###########################################
+############################################
+############################################
+############################################
